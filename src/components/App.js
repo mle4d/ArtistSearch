5c2c2be1152fa5dd320e7e224320faa3b8b23d48
emily <mle4d@me.com>
2019-08-21 13:59:11 -0700

    init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router
+  Route
+} from 'react-router-dom';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+export default function App() {
+  return (
+<Router>
+  <Route path="/home" component={Home} />
+</Router>
+  )
+}
+  
