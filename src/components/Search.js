6ba370c64cd0df42190a4437b47405f5c0f394c7
emily <mle4d@me.com>
2019-08-22 15:40:23 -0700

    search work!

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,32 +9,35 @@ export default class Search extends PureComponent {
     count: 1,
     offset: 0
   }
+
   handleChange = ({ target }) => {
     this.setState({ [target.name ]: target.value });
   }
-fetchArtist = (event) => {
-  searchArtist(this.state.artistSearch)
-    .then(res => {
-      this.setState({
-        artist: res.artist,
-        count: res.count,
-        offset: res.offset
+
+  fetchArtist = (event) => {
+    searchArtist(this.state.artistSearch)
+      .then(res => {
+        this.setState({
+          artist: res.artist,
+          count: res.count,
+          offset: res.offset
+        });
       });
-    });
-}
-render() {
-	const { artist, artistSearch, count, offset } = this.state;
-	return (
-				<>
-					<h1>Search for Artist</h1>
-						<form onSubmit={this.fetchArtist}>
-							<section>
-								<input type="text" name="artistSearch" value="artistSearch" onChange={this.handleChange}></input>
-							</section>
-							<button>Search</button>
-						</form>
-				</>
-	);
-}
+  }
+
+  render() {
+    const { artist, artistSearch, count, offset } = this.state;
+    return (
+          <>
+            <h1>Search for Artist</h1>
+              <form onSubmit={this.fetchArtist}>
+                <section>
+                  <input type="text" name="artistSearch" value={this.state.artistSearch} onChange={this.handleChange}></input>
+                </section>
+                <button>Search</button>
+              </form>
+          </>
+    );
+  }
 
 }
