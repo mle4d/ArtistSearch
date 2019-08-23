f85b293c661901742a3e9cc41110855724e82d97
emily <mle4d@me.com>
2019-08-23 15:06:18 -0700

    I need halp

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import {
   BrowserRouter as Router, 
   Route } from 'react-router-dom';
 import Search from './Search';
-import Artists
+
+
 
 
 export default function App() {
   return (
     <Router>
       <Route exact path="/" component={Search} />
-      {/* <Route exact path="/" component={Artist} /> */}
     </Router>
   );
 }
