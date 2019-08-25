399d48a11d4eb8b3345d633a32855eab5c91709f
emily <mle4d@me.com>
2019-08-24 20:16:51 -0700

    artist results

--- src/components/containers/ArtistResults.js
+++ src/components/containers/ArtistResults.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
-import Artists from '../Artists';
-import { searchArtist } from '../../services/musicAPI';
+import PropTypes from 'prop-types';
+import Releases from '../components/releases/Releases';
+import { artistInfo } from '../service/musicApi';
+// import { withPaging } from '../components/withPaging';
 
 class ArtistResults extends Component {
-  state = {
-    artists: []
-  };
-  
-  fetch = () => {
-    searchArtist(this.state.artist)
-      .then(({ artists }) => {
-        this.setState({ artists });
-      }); 
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    page: PropTypes.number.isRequired,
+    setAllPages: PropTypes.func.isRequired
   }
-  changeInput = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  }
-  handleSearch = () => {
-    event.preventDefault();
-    this.fetch(); 
-  }
-  
-  render() {
 
+  state = {
+    releases: []
+  }
+
+  fetch = () => {
+    artistInfo(this.props.match.params.id, this.props.page)
+      .then(({ releases, allPages }) => {
+        this.setState({ releases });
+        this.props.setAllPages(allPages);
+      });
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.page !== this.props.page) {
+      this.fetch();
+    }
+  }
+
+  render() {
+    const { releases } = this.state;
     return (
-    <>
-      <Artists artists={this.state.artists}/>
-    </>
+      <>
+        <h1>Releases for {this.props.match.params.artistName}</h1>
+        <Releases releases={releases} />
+      </>
     );
   }
 }
-export default ArtistResults;
+
+export default withPaging(ArtistResults);
